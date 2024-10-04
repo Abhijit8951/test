82e1b9176448d40485b94f77fc142b4977783201
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-04 23:25:51 +0530

    added javascript contents

--- joke.js
+++ joke.js
@@ -2,3 +2,16 @@ let jokepara = document.querySelector("#joke");
 let btn = document.querySelector("#btn");
 
 let url = "https://v2.jokeapi.dev/joke/Any?safe-mode";
+
+async function getjokes() {
+    try {
+        let res = await axios.get(url);
+        jokepara.innerText = res.data.setup + res.data.delivery || res.data.joke;
+    } catch(e) {
+        jokepara.innerText = "Error occured" + e;
+    }
+}
+
+btn.addEventListener("click", () => {
+    getjokes();
+});
